9cd0fc40ead297e74aa076ade1f9f8713542aab3
unknown <danang1724@gmail.com>
2023-08-23 18:14:35 +0700

    update

---
+++ application/controllers/Home.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Tagihan_kontrak_admin/Taggihan_kontrak_admin_model');
+        $this->load->model('Admin/Data_kontrak_model');
+        $this->load->model('Auth_model');
+        $session = $this->session->userdata('id_pegawai');
+        // if (!$session) {
+        // 	redirect('auth');
+        // }
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Admin/Dashboard';
+        // januari
+        $get_pegawai = $this->Auth_model->get_pegawai();
+        $data['id_departemen'] = $get_pegawai['id_departemen'];
+        $data['id_area'] = $get_pegawai['id_area'];
+        $data['id_sub_area'] = $get_pegawai['id_sub_area'];
+        $data['get_departemen'] = $this->Data_kontrak_model->get_departemen($get_pegawai['id_departemen']);
+        $data['get_area'] = $this->Data_kontrak_model->get_area($get_pegawai['id_area']);
+        $data['get_sub_area'] = $this->Data_kontrak_model->get_sub_area($get_pegawai['id_sub_area']);
+        $this->load->view('template_stisla/header');
+        $this->load->view('template_stisla/sidebar', $data);
+        $this->load->view('home/index', $data);
+        $this->load->view('template_stisla/footer',);
+        $this->load->view('admin/dashboard/ajax');
+    }
+}
---
+++ application/views/home/index.php
@@ -0,0 +1,54 @@
+<style>
+    th,
+    td {
+        white-space: nowrap;
+    }
+
+    div.dataTables_wrapper {
+        width: 800px;
+        margin: 0 auto;
+    }
+
+    tr {
+        height: 50px;
+    }
+</style>
+<div class="main-content" style="font-family: 'RNSSanz-Light'">
+    <section class="section">
+        <nav class="navbar navbar-expand-lg main-navbar" style="background-color:#fce49c;height:50px;
+  position: fixed; top:50px;  padding-bottom: -10px;">
+            <b style="margin-left: auto; font-weight:1000" class="text-black">Home</b>
+        </nav>
+
+
+        <div class="card" style="margin-top: 60px; padding: 20px;background: rgb(36,93,120);
+background: linear-gradient(188deg, rgba(36,93,120,1) 47%, rgba(1,118,205,1) 92%); color:white">
+            <h2>Selamat Datang di Aplikasi Digitalisasi Proses Kerja Kontrak Manajemen Di dalam Aplikasi ini terdapat 4 Modul Utama</h2>
+        </div>
+
+        <div class="card" style="margin-top: 20px; padding: 30px;background: rgb(36,93,120);
+background: linear-gradient(188deg, rgba(36,93,120,1) 47%, rgba(1,118,205,1) 92%); color:white">
+            <h6>MODUL 1 - MODUL KONTRAK MANAJEMEN</h6>
+            <label for="">Modul ini digunakan dalam membuat Data Terkait Informasi Kontrak Manajemen yang sudah ada</label>
+        </div>
+
+
+        <div class="card" style="margin-top: 20px; padding: 30px;background: rgb(36,93,120);
+background: linear-gradient(188deg, rgba(36,93,120,1) 47%, rgba(1,118,205,1) 92%); color:white">
+            <h6>MODUL 2 - MODUL KONTRAK MANAJEMEN</h6>
+            <label for="">Modul ini digunakan untuk menyusun Administrasi Penyedia Jasa yang terdiri dari Pra Pengadaan dan Pasca Pengadaan</label>
+        </div>
+
+        <div class="card" style="margin-top: 20px; padding: 30px;background: rgb(36,93,120);
+background: linear-gradient(188deg, rgba(36,93,120,1) 47%, rgba(1,118,205,1) 92%); color:white">
+            <h6>MODUL 3 - MODUL TAGIHAN PENYEDIA JASA</h6>
+            <label for="">Modul ini digunakan untuk menyusun Tagihan Penyedia Jasa dan Tracking Tagihan Penyedia Jasa</label>
+        </div>
+
+        <div class="card" style="margin-top: 20px; padding: 30px;background: rgb(36,93,120);
+background: linear-gradient(188deg, rgba(36,93,120,1) 47%, rgba(1,118,205,1) 92%); color:white">
+            <h6>MODUL 4 - MODUL ANALISIS DATA</h6>
+            <label for="">Modul ini digunakan untuk melihat Analisis Data dari Data - Data yang sudah diinput di Modul Sebelumnya</label>
+        </div>
+    </section>
+</div>
